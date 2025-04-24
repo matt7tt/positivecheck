c9bcd20d431a6fa790610abeee49d0612dde8668
Matt Thorn <matt7tt@users.noreply.github.com>
2025-04-24 16:01:19 -0700

    add fb ad code

--- components/FacebookPixel.tsx
+++ components/FacebookPixel.tsx
@@ -12,30 +12,45 @@ declare global {
 
 export default function FacebookPixel() {
   useEffect(() => {
-    // Facebook Pixel Code with TypeScript fixes
-    (function(f, b, e, v, n, t, s) {
-      if ((f as any).fbq) return;
-      n = (f as any).fbq = function() {
-        (n as any).callMethod ? 
-          (n as any).callMethod.apply(n, arguments) : 
-          (n as any).queue.push(arguments);
-      };
-      if (!(f as any)._fbq) (f as any)._fbq = n;
-      (n as any).push = n;
-      (n as any).loaded = true;
-      (n as any).version = '2.0';
-      (n as any).queue = [];
-      t = b.createElement(e);
-      t.async = true;
-      t.src = v;
-      s = b.getElementsByTagName(e)[0];
-      if (s && s.parentNode) {
-        s.parentNode.insertBefore(t, s);
-      }
-    })(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+    // Facebook Pixel Code with proper TypeScript typing
+    const f = window;
+    const b = document;
+    const e = 'script';
+    const v = 'https://connect.facebook.net/en_US/fbevents.js';
     
-    window.fbq('init', '2093713827815363');
-    window.fbq('track', 'PageView');
+    // Using traditional approach instead of IIFE to avoid TypeScript errors
+    if (f.fbq) return;
+    
+    // Define fbq function
+    f.fbq = function() {
+      const obj = f.fbq as any;
+      obj.callMethod ? 
+        obj.callMethod.apply(obj, arguments) : 
+        obj.queue.push(arguments);
+    };
+    
+    // Setup fbq properties
+    if (!f._fbq) f._fbq = f.fbq;
+    const fbq = f.fbq as any;
+    fbq.push = fbq;
+    fbq.loaded = true;
+    fbq.version = '2.0';
+    fbq.queue = [];
+    
+    // Create script element
+    const t = b.createElement(e) as HTMLScriptElement;
+    t.async = true;
+    t.src = v;
+    
+    // Insert script into DOM
+    const s = b.getElementsByTagName(e)[0];
+    if (s && s.parentNode) {
+      s.parentNode.insertBefore(t, s);
+    }
+    
+    // Initialize pixel and track page view
+    f.fbq('init', '2093713827815363');
+    f.fbq('track', 'PageView');
   }, []);
 
   return (
